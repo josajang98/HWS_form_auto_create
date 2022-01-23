ef2b70414d0661c24eb6ca213db6b392165d6a8f
josajang98 <na0173@naver.com>
2022-01-23 11:57:58 +0900

    기능 추가:touch_today_file 함수 작성

--- main.py
+++ main.py
@@ -4,8 +4,21 @@ from datetime import datetime
 today = datetime.today().strftime('%m%d')
 
 HWS_NAME = 'HWS'
+HANDOUT_MINE_NAME = 'handout_mine'
 HWM_PATH = f'../{HWS_NAME}/{today}/'
+HANDOUT_MINE_PATH = f'../{HANDOUT_MINE_NAME}/{today}/hws/python_04_homework.pdf'
 
 
 def mkdir_today():
     os.mkdir(HWM_PATH)
+
+
+def touch_today_file():
+    HW_name = today+'_homework.md'
+    WS_name = today+'_workshop.md'
+
+    f = open(HWM_PATH+HW_name, 'w')
+    f.close()
+
+    f = open(HWM_PATH+WS_name, 'w')
+    f.close()
