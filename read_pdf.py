abf5613570d26f6347446b514eb621f60d022153
josajang98 <na0173@naver.com>
2022-01-23 11:59:35 +0900

    기능 추가: pdfminer 모듈 사용으로 pdf읽기

---
+++ read_pdf.py
@@ -0,0 +1,37 @@
+from io import StringIO
+from pdfminer.pdfpage import PDFPage
+from pdfminer.layout import LAParams
+from pdfminer.converter import TextConverter
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from datetime import datetime
+
+today = datetime.today().strftime('%m%d')
+HANDOUT_MINE_NAME = 'handout_mine'
+HANDOUT_MINE_PATH = f'../{HANDOUT_MINE_NAME}/{today}/hws/python_04_homework.pdf'
+
+
+def convert_pdf_to_txt(HANDOUT_MINE_PATH):
+    rsrcmgr = PDFResourceManager()
+    retstr = StringIO()
+    codec = 'utf-8'
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+    fp = open(HANDOUT_MINE_PATH, 'rb')
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    password = ""
+    maxpages = 0
+    caching = True
+    pagenos = set()
+
+    for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password, caching=caching, check_extractable=True):
+        interpreter.process_page(page)
+
+    text = retstr.getvalue()
+
+    fp.close()
+    device.close()
+    retstr.close()
+    return text
+
+
+print(convert_pdf_to_txt(HANDOUT_MINE_PATH))
